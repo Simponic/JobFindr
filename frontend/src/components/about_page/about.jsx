98776d5e16b930d128d72c0cb32020b9ab16f00d
Hailey Dennis <a02242676@usu.edu>
2022-03-05 10:46:17 -0700

    added about page

---
+++ frontend/src/components/about_page/about.jsx
@@ -0,0 +1,15 @@
+
+export const About = () => {
+
+  return (
+  <div>
+    <h1 className='mt-5 mb-5'> About JobFind </h1>
+    <div className="w-50">
+    <p> JobFind is the app for customers who need lawn care and for workers who are looking for lawn care work. </p>
+    <p> Our app is simple to use. Customers can create a job by selecting the service they need (lawn mowing, shoveling, etc.), setting the date and time, and adding any specific instructions. Then, our app will automatically match them with a worker who is available and qualified to do the job. </p>
+    <p> Workers love JobFind because it is a great way to find work in their local area. They can see what jobs are available, and they can choose the jobs that they are interested in. </p>
+    <p className='fst-italic'> We are not committed to providing a safe and easy way for customers and workers to connect. We do not screen all workers, and we do not provide insurance and payment protection for customers. </p>
+    </div>
+   </div>
+  );
+}
