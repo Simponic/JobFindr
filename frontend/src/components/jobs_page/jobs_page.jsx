de1d69cc0aff8d558547b12254bdd7d270896165
Hailey Dennis <a02242676@usu.edu>
2022-03-15 14:45:10 -0600

    setup job listings, ready to map

--- frontend/src/components/jobs_page/jobs_page.jsx
+++ frontend/src/components/jobs_page/jobs_page.jsx
@@ -1,112 +1,37 @@
-import { ListGroup, Container, Button } from 'react-bootstrap';
+import { ListGroup, Container, Button, Col } from 'react-bootstrap';
 import { FaLongArrowAltRight } from 'react-icons/fa';
 import { GiGrass } from 'react-icons/gi';
 
 export const JobsPage = () => {
+  let jobs = ['replace', 'with', 'actual', 'jobs', 'a', 'b', 'c', 'd'];
   return (
-    <Container>
+    <Container className="flex">
       <div className="mt-5"></div>
       <h1 className="text-2xl mt-5 mb-3 logo-color">Jobs</h1>
       <div className="job-scroll scrollbar-primary">
+
         <ListGroup as="ul">
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <p className="jobtype">Lawn Mowing <GiGrass/></p>
-            <div>
-              <div className="job-title">100 acre lawn $30 compensation asdf asdf asdfasd fasdf asdfasdf asdfasdf asdfasdfasdf</div>
-              <Container className="flex-col">
-                <div className="job-listing">
-                  <Button variant="secondary">
-                    <FaLongArrowAltRight />
-                  </Button>
-                </div>
-                <p className="job-listing">Status: Assigned</p>
-              </Container>
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-          >
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">Subheading</div>
-              Cras justo odio
-            </div>
-          </ListGroup.Item>
+          {jobs.map((job) => (
+            <ListGroup.Item
+              key={job} // Change to job.id
+              as="li"
+              className="d-flex justify-content-between align-items-start m-2">
+              <div className="pt-4">
+                <GiGrass className='list-icons'/>
+              </div>
+              <div>
+                <div className="job-title">100 acre lawn $30 compensation asdf asdf asdfasd fasdf asdfasdf asdfasdf asdfasdfasdf</div>
+                <Container className="flex-col">
+                  <div className="job-listing">
+                    <Button variant="secondary">
+                      <FaLongArrowAltRight />
+                    </Button>
+                  </div>
+                  <span className="job-listing">Status: Assigned</span>
+                </Container>
+              </div>
+            </ListGroup.Item>
+          ))}
         </ListGroup>
       </div>
     </Container>
