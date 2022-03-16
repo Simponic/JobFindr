698d7dc631877780cb0443cf7aad91badc54f96c
Hailey Dennis <a02242676@usu.edu>
2022-03-16 15:20:05 -0600

    dynamic icon loading

--- frontend/src/components/jobs_page/jobs_page.jsx
+++ frontend/src/components/jobs_page/jobs_page.jsx
@@ -1,9 +1,39 @@
-import { ListGroup, Container, Button, Col } from 'react-bootstrap';
+import { ListGroup, Container, Button } from 'react-bootstrap';
 import { FaLongArrowAltRight } from 'react-icons/fa';
-import { GiGrass } from 'react-icons/gi';
+import { useState, useEffect } from 'react';
 
 export const JobsPage = () => {
-  let jobs = ['replace', 'with', 'actual', 'jobs', 'a', 'b', 'c', 'd'];
+  let CurrIcon = null;
+
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    // Api call to get jobs (depends on worker or customer)
+    // Set jobs
+  });
+
+  // Only take from gi library
+  const icons = require('react-icons/gi');
+
+  let testJobs = ['replace', 'with', 'actual', 'jobs', 'a', 'b', 'c', 'd'];
+
+
+  // Won't need this once we can access job.jobtype.icon
+  const job_type_icons = ['GiGrass', 'GiSpade', 'GiRake', 'GiHammerNails', 'GiSnowing', 'GiBriefcase', 'GiBeerStein', 'GiCigar']
+
+  // Also won't need this
+  const getIcon = (num) => {
+    let iconName = job_type_icons[num];
+    CurrIcon = icons[iconName]; 
+  }
+
+  // Replace the function above with this one, then just call getIcon() below
+  // const getIcon = () => {
+  //   CurrIcon = icons[job.jobtype.icon] 
+  // }
+  
+
+
   return (
     <Container className="flex">
       <div className="mt-5"></div>
@@ -11,13 +41,14 @@ export const JobsPage = () => {
       <div className="job-scroll scrollbar-primary">
 
         <ListGroup as="ul">
-          {jobs.map((job) => (
+          {testJobs.map((job) => (
             <ListGroup.Item
               key={job} // Change to job.id
               as="li"
               className="d-flex justify-content-between align-items-start m-2">
               <div className="pt-4">
-                <GiGrass className='list-icons'/>
+              {getIcon(testJobs.indexOf(job)) /* Change to just getIcon() */}
+              <CurrIcon className="list-icons"/>
               </div>
               <div>
                 <div className="job-title">100 acre lawn $30 compensation asdf asdf asdfasd fasdf asdfasdf asdfasdf asdfasdfasdf</div>
