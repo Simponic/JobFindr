528668c8dc93afc884792b9b0a30cf480d0c0b5f
James Field <jwcf1234@gmail.com>
2022-03-18 16:05:11 -0600

    started this branch

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from authentication.models import User, Worker
 from django.conf import settings
+import math
 
 class Status(models.TextChoices):
     AVAILABLE = 'available'
@@ -13,11 +14,6 @@ class JobType(models.Model):
     job_type = models.CharField(max_length=50, null=False)
     icon = models.CharField(max_length=100, null=False)
     archived = models.BooleanField(null=False, default=False)
-
-    def create_new_job_type(self, jobType, icon):
-        self.jobType = jobType
-        self.available = True
-        self.icon = icon
 
 
 class Job(models.Model):
@@ -40,3 +36,27 @@ class WorkerJobTimes(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     start_time = models.BigIntegerField(default=0, null=False)
     end_time = models.BigIntegerField(default=0, null=False)
+
+    def assign_job(self):
+        # creating variables to make code in future less confusing when doing the math
+        userLat = self.job.latitude / 57.29577951
+        userLong = self.job.longitude / 57.29577951
+        workerLat = self.worker.user.home_latitude / 57.29577951
+        workerLong = self.worker.user.home_longitude / 57.29577951
+        # 57.29577951 is approximately 180/PI, so converts values from degrees to radians
+
+        Distance = 3963.0 * math.arccos[(math.sin(userLat) * math.sin(workerLat)) + math.cos(userLat) *
+                                        math.cos(workerLat) * math.cos(workerLong - userLong)]
+
+        # formula from https://www.geeksforgeeks.org/program-distance-two-points-earth/
+
+        Distance = abs(Distance)
+
+        if Distance < 25:
+            for i in self.WorkerAvailability
+
+            self.job.status = 'assigned'
+
+
+
+
--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from jobs.models import JobType, Job
-from authentication.models import Role, User
+from authentication.models import Role, User, Worker, WorkerAvailability
 import json
 from authentication.views import get_user_or_error
 
@@ -14,23 +14,37 @@ def create_job(request, id):
     else:
         return JsonResponse(user_error_tup)
 
-    body = json.loads(request.body.decode('utf-8'))
+    if (request.method == "GET"):
+        resp = {'success': True, 'user': }
 
-    job = Job(
-        user_id=user_error_tup['user'].id,
-        job_type=body['job_type'],
-        price=body['price'],
-        time_estimate=body['time_estimate'],
-        start_time=body['start_time'],
-        end_time=body['end_time'],
-        address=user.home_address,
-        latitude=user.home_latitude,
-        longitude=user.home_longitude,
-        comment=body['comment'],
-    )
+        return JsonResponse(resp)
 
-    job.save()
+    elif (request.method == "PUT"):
+        body = json.loads(request.body.decode('utf-8'))
 
+        job = Job(
+            user=user.id,
+            job_type=body['job_type'],
+            price=body['price'],
+            time_estimate=body['time_estimate'],
+            start_time=body['start_time'],
+            end_time=body['end_time'],
+            address=user.home_address,
+            latitude=user.home_latitude,
+            longitude=user.home_longitude,
+            comment=body['comment'],
+        )
+
+        for i in Worker:
+            i.assign_job(job)
+            if job.status == 'assigned':
+                break
+        if job.status != 'assigned':
+            job.save()
+            return {'success': False, 'message': 'No available workers at this time for this job'}
+
+        job.save()
+        return {'success': True, 'message': 'A worker is on their way'}
 
 def create_new_job_type(request, id):
     user_error_tup = get_user_or_error(request)
