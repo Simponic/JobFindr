9b9997a7a01732665aee8344249e9b080b42cfda
Logan Hunt <loganhunt@simponic.xyz>
2022-03-20 19:59:43 -0600

    Update UML and also fix when user not signed in

--- backend/contactform/views.py
+++ backend/contactform/views.py
@@ -9,17 +9,14 @@ def create_new_submission(request):
     body = json.loads(request.body.decode('utf-8'))
     user_error_tup = get_user_or_error(request)
     
-    if (user_error_tup['success']):
-        user = user_error_tup['user']
-    else:
-        return JsonResponse(user_error_tup)
-
     submission = Submission(
-        user=user,
         body=body['body'],
         email=body['email'],
         status=Status.OPEN,
     )
+
+    if ('user' in user_error_tup):
+        submission.user = user_error_tup['user']
 
     if ('job_id' in body):
         try:
