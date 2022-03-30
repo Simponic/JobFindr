b1c7221c5ffc5f924f25baca27b52f0c2c5b15f0
Hailey Dennis <a02242676@usu.edu>
2022-03-29 21:11:25 -0600

    changed owner portal from flex to grid

--- frontend/src/components/owner_portal/owner_portal.jsx
+++ frontend/src/components/owner_portal/owner_portal.jsx
@@ -4,68 +4,76 @@ import { FaArrowRight} from 'react-icons/fa';
 import { AuthContext } from "../../services/auth";
 import { useContext } from "react";
 import { NotFound } from "../../components/errors/not_found";
+import { Row, Col } from "react-bootstrap";
 
 
 export const OwnerPortal = () => {
   const navigate = useNavigate();
   const auth = useContext(AuthContext);
 
-
   return ( 
     <Container>
-      {auth.user.role === 'owner' ?
-      <><h1 className="mt-5">Site Tools</h1><p>Welcome! This portal gives you access to manage site users, jobs, and contact forms.</p><p>Select one of the options below.</p>
-      <div className="portal-cards">
-          <Card className="card">
-            <Card.Img variant="top" height="250vh" src={process.env.PUBLIC_URL + '/portal_images/conference-128.png'} alt="users icon"/>
-            <Card.Body className="card-desc">
+    {auth.user.role === 'owner' ?
+    <><h1 className="mt-5">Site Tools</h1><p>Welcome! This portal gives you access to manage site users, jobs, and contact forms.</p><p>Select one of the options below.</p>
+    <Row>
+        <Col md={2} sm={3}>
+          <Card className="card-shadow">
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + '/portal_images/conference-128.png'} alt="users icon"/>
+            <Card.Body>
               <Card.Title>Site Users</Card.Title>
-              <Card.Text>
-                A list of all users on the site. Click on a user to view and edit their profile.
+              <Card.Text className="text-height">
+                A list of all users on the site. Click on a user to view and edit their profile. 
               </Card.Text>
-              <Button className="card-button" variant="secondary" onClick={() => {navigate('/owner/view-users')}}><FaArrowRight /></Button>
+              <Button className="portal-btn" variant="secondary" onClick={() => {navigate('/owner/view-users')}}><FaArrowRight /></Button>
             </Card.Body>
           </Card>
+        </Col>
 
-          <Card className="card">
-            <Card.Img variant="top" height="250vh" src={process.env.PUBLIC_URL + '/portal_images/clipboard-8-128.png'} alt="contact form icon" />
-            <Card.Body className="card-desc">
+        <Col md={2} sm={3}>
+          <Card className="card-shadow">
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + '/portal_images/clipboard-8-128.png'} alt="contact form icon" />
+            <Card.Body>
               <Card.Title>Contact Forms</Card.Title>
-              <Card.Text>
-                A list of all contact forms submitted to the site. 
+              <Card.Text className="text-height">
+                A list of all contact forms submitted to the site. {'\u00A0'.repeat(30)}
               </Card.Text>
-              <Button className="card-button" variant="secondary" onClick={() => {navigate('/owner/view-forms')}}><FaArrowRight /></Button>
+              <Button className="portal-btn" variant="secondary" onClick={() => {navigate('/owner/view-forms')}}><FaArrowRight /></Button>
             </Card.Body>
           </Card>
+        </Col>
 
-          <Card className="card">
-            <Card.Img variant="top" height="250vh" src={process.env.PUBLIC_URL + '/portal_images/view-details-128.png'} alt="job listings icon" />
-            <Card.Body className="card-desc">
+        <Col md={2} sm={3}>
+          <Card className="card-shadow">
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + '/portal_images/view-details-128.png'} alt="job listings icon" />
+            <Card.Body>
               <Card.Title>Job Listings</Card.Title>
-              <Card.Text>
-                A list of all jobs posted to the site. Click on a job to view and edit it.
+              <Card.Text className="text-height">
+                A list of all jobs posted to the site. Click on a job to view and edit it. {'\u00A0'.repeat(6)}
               </Card.Text>
-              <Button className="card-button" variant="secondary"><FaArrowRight /></Button>
+              <Button className="portal-btn" variant="secondary"><FaArrowRight /></Button>
             </Card.Body>
           </Card>
+        </Col>
 
-          <Card className="card">
-            <Card.Img variant="top" height="250vh" src={process.env.PUBLIC_URL + '/portal_images/rake-128.png'} alt="job type icon" />
-            <Card.Body className="card-desc">
+        <Col md={2} sm={3}>
+          <Card className="card-shadow">
+            <Card.Img variant="top" src={process.env.PUBLIC_URL + '/portal_images/rake-128.png'} alt="job type icon" />
+            <Card.Body>
               <Card.Title>Job Types</Card.Title>
-              <Card.Text>
-                A tool to create, edit, or archive job types.
+              <Card.Text className="text-height">
+                A management tool to create, edit, or archive job types. {'\u00A0'.repeat(24)}
               </Card.Text>
-              <Button className="card-button" variant="secondary"><FaArrowRight /></Button>
+            <Button className="portal-btn" variant="secondary"><FaArrowRight /></Button>
             </Card.Body>
           </Card>
+        </Col>
 
 
-        </div></>
-      : <NotFound />
-      } 
-      
+      </Row></>
+    : <NotFound />
+    } 
+    
 
-    </Container>
+  </Container>
   )
 }
