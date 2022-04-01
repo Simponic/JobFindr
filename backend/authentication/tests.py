defd9bdef21d6a73726ef507de06cbece5ec4354
Logan Hunt <loganhunt@simponic.xyz>
2022-03-31 20:33:32 -0600

    Fix James' tests

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -4,9 +4,8 @@ from authentication.models import User, Role, Worker
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(name="kev",
+        kev = User.objects.create(name="kev",
                             email="kev@gmail.com",
-                            password="passwordkev",
                             avatar="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png",
                             phone_number="3853378828",
                             home_address="1265 n 400 e logan utah 84341",
@@ -15,10 +14,11 @@ class UserTestCase(TestCase):
                             balance=100,
                             role=Role.CUSTOMER,
                             )
+        kev.set_password("passwordkev")
+        kev.save()
 
-        User.objects.create(name="Bob",
+        bob = User.objects.create(name="Bob",
                             email="Bob@gmail.com",
-                            password="passwordbob",
                             avatar="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-674010.jpg&fm=jpg",
                             phone_number="(385) 337-8829",
                             home_address="1265 n 400 e logan utah 84341",
@@ -27,10 +27,12 @@ class UserTestCase(TestCase):
                             balance=250,
                             role=Role.CUSTOMER,
                             )
+        bob.set_password("passwordbob")
+        bob.save()
 
-        User.objects.create(name="Worker1",
+
+        workerone = User.objects.create(name="Worker1",
                             email="Worker1@gmail.com",
-                            password="Worker1",
                             avatar="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png",
                             phone_number="3853378830",
                             home_address="1265 n 400 e logan utah 84341",
@@ -39,10 +41,11 @@ class UserTestCase(TestCase):
                             balance=250,
                             role=Role.WORKER,
                             )
+        workerone.set_password("Worker1")
+        workerone.save()
 
-        User.objects.create(name="Site Owner",
+        owner = User.objects.create(name="Site Owner",
                             email="siteOwner@gmail.com",
-                            password="GJ/n(sTDp4#63jn@",
                             phone_number="4353432428",
                             home_address="4205 Old Main Hill Logan UT 84322",
                             home_latitude=41.34176,
@@ -50,33 +53,33 @@ class UserTestCase(TestCase):
                             balance=1000,
                             role=Role.OWNER,
         )
+        owner.set_password("helloworld")
+        owner.save()
 
-        worker1 = User.objects.get(name="Worker1")
-
-        Worker.objects.create(user=worker1)
+        Worker.objects.create(user=workerone)
 
     def test_user_password_true(self):
         kevin = User.objects.get(name="kev")
 
-        self.assertTrue(User.compare_password(kevin, "passwordkev"))
+        self.assertTrue(kevin.compare_password("passwordkev"))
 
     def test_user_password_false(self):
         kevin = User.objects.get(name="kev")
 
-        self.assertFalse(User.compare_password(kevin, "wrong"))
+        self.assertFalse(kevin.compare_password("wrong"))
 
     def test_other_user_password(self):
         kevin = User.objects.get(name="kev")
-        bob = User.objects.get(name="Bob")
 
-        self.assertFalse(User.compare_password(kevin, "passwordbob"))
+        self.assertFalse(kevin.compare_password("passwordbob"))
 
     def test_change_password(self):
         kevin = User.objects.get(name="kev")
 
         kevin.set_password("password2")
+        kevin.save()
 
-        self.assertTrue(User.compare_password(kevin, "password2"))
+        self.assertTrue(kevin.compare_password("password2"))
 
 
     def test_strip_phone_number(self):
